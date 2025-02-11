c82536553a1fee811933b31fd2ab9b8c9b357fcc
Jason Tan <jtan20040401@gmail.com>
2025-02-11 21:54:18 +1100

    create oops page

---
+++ src/test/java/com/bw/petclinic/controller/OopsControllerUnitTest.java
@@ -0,0 +1,36 @@
+package com.bw.petclinic.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+@WebMvcTest(OopsController.class)
+public class OopsControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testOops() throws Exception {
+        mockMvc
+                .perform(get("/oops"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error"))
+                .andExpect(model().attribute("error", "Expected OOPS Exception"));
+    }
+
+    @Test
+    public void testCatchAll() throws Exception {
+        mockMvc
+                .perform(get("/catch-all"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error"))
+                .andExpect(model().attribute("error", "Internal Error [No static resource catch-all.]"));
+    }
+
+}
