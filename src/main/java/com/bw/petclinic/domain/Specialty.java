01c0e91741afe85d1b7553a68cfc3aee4e121ee1
Jason Tan <jtan20040401@gmail.com>
2025-02-14 22:03:38 +1100

    added vet microservice

---
+++ src/main/java/com/bw/petclinic/domain/Specialty.java
@@ -0,0 +1,16 @@
+package com.bw.petclinic.domain;
+
+import lombok.Data;
+
+@Data
+public class Specialty {
+
+    private Integer id;
+
+    private String name;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/main/java/com/bw/petclinic/domain/Vet.java
@@ -0,0 +1,18 @@
+package com.bw.petclinic.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Vet {
+
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private List<Specialty> specialties;
+
+}
