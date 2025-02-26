69becee72db04210ef21aa498598f07a0c07e230
Jason Tan <jtan20040401@gmail.com>
2025-02-26 16:35:25 +1100

    implement visit new and add

--- src/main/java/com/bw/petclinic/domain/Visit.java
+++ src/main/java/com/bw/petclinic/domain/Visit.java
@@ -1,5 +1,7 @@
 package com.bw.petclinic.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -9,8 +11,12 @@ public class Visit {
 
     private Integer id;
 
+    @NotNull(message = "Visit date must not be null")
     private LocalDate visitDate;
 
+    @NotBlank(message = "Visit description must not be blank")
     private String description;
+
+    private Integer petId;
 
 }
